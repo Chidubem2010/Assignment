47f072300a6a9e65f837e8c1a9f6a0edc162d14d
Dubem <daimianpriest@gmail.com>
2023-08-15 22:33:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let list = ['The Number List',1,2,3,4,5,6,7,8,9,10];
+let evenList = [0,2,4,6,8,10];
+let oddList = ['The Odd Number LIst',1,3,5,7,9,11];
+if (list) {
+    console.log(list)
+}
+if (oddList) {
+    console.log(oddList);
+}
